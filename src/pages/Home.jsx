18a039ce4f94b8e2478857a33b768bdcf01e5d4e
marinadotnet <msichova@outlook.com>
2025-06-18 11:35:01 +0800

    Added home page content.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,82 @@
+import {NumberedCodeBloc, CodeKeyWords, CodeValues, ClassName, Tab, SingleComment} from "../components/CodeTokens.jsx";
+
 export default function Home(){
     return (
-        <>
-        <h1>Home from Home page</h1>
-        <h1>Home from Home page</h1>
-        <h1>Home from Home page</h1>
-        <h1>Home from Home page</h1>
-        <h1>Home from Home page</h1>
-        <h1>Home from Home page</h1>
-        </>
+        <NumberedCodeBloc children={[
+            <><CodeKeyWords>using</CodeKeyWords> CleanCode;</>,
+            <><CodeKeyWords>using</CodeKeyWords> SoftwareArchitecture.SOLID;</>,
+            <><CodeKeyWords>using</CodeKeyWords> DesignPatterns.*;</>,
+            <><CodeKeyWords>using</CodeKeyWords> DevMindset.Focus;</>,
+            <br/>,
+            <SingleComment>Welcome to my Portfolio Web Pages.</SingleComment>,
+            <SingleComment>I'm Junior Web/Software Developer.</SingleComment>,
+            <br />,
+            <><CodeKeyWords>namespace</CodeKeyWords> MarinaDotNet;</>,
+            <br />,
+            <><CodeKeyWords>class</CodeKeyWords> <ClassName>Program</ClassName></>,
+            <>&#123;</>,
+            <><Tab number={2} /><CodeKeyWords>static void</CodeKeyWords> <ClassName>Main</ClassName>(<CodeKeyWords>string</CodeKeyWords>[] args)</>,
+            <><Tab number={2} />&#123;</>,
+            <><Tab number={3} /> <CodeKeyWords>var</CodeKeyWords> developer = <CodeKeyWords> new </CodeKeyWords>Developer()</>,
+            <><Tab number={3} /> &#123;</>,
+            <><Tab number={6} />Name = <CodeValues>"Marina Sichova"</CodeValues>,</>,
+            <br />,
+            <><Tab number={6} />Traits = [
+                <CodeValues>".NET Developer"</CodeValues>,
+                <CodeValues>"Backend"</CodeValues>,
+                <CodeValues>"Data-Driven"</CodeValues>,
+                <CodeValues>"Performance Optimizer"</CodeValues>,
+                <CodeValues>"Team Player"</CodeValues>,
+                <CodeValues>"Problem Solver"</CodeValues>,
+                <CodeValues>"Detail-Orientated"</CodeValues>,
+                <CodeValues>"Innovative Thinker"</CodeValues>,
+                <CodeValues>"Lifelong Learner"</CodeValues>,
+                <CodeValues>"Adaptable"</CodeValues>],
+            </>,
+            <br />,
+            <><Tab number={6} />Passion = <CodeValues> "Building robust, maintaible web and desktop apps with .NET and exploring front-end with React"</CodeValues>,</>,
+            <br />,
+            <><Tab number={6} />Certifications = [
+                <CodeValues>"Diploma of Information Technology(advanced programming)</CodeValues>,
+                <CodeValues>"Diploma of Business"</CodeValues>],
+            </>,
+            <br />,
+            <><Tab number={6} />Stack = [
+                <CodeValues>"C#"</CodeValues>,
+                <CodeValues>"ASP.NET Core"</CodeValues>,
+                <CodeValues>"Entity Framework"</CodeValues>,
+                <CodeValues>"SQL"</CodeValues>,
+                <CodeValues>"NoSql"</CodeValues>,
+                <CodeValues>"React"</CodeValues>,
+                <CodeValues>"GitHub"</CodeValues>,
+                <CodeValues>"Unit Testing"</CodeValues>,
+                <CodeValues>"Java"</CodeValues>,
+                <CodeValues>"Python"</CodeValues>,
+                <CodeValues>"JavaScript"</CodeValues>],
+            </>,
+            <br />,
+            <><Tab number={6} />Experience = <CodeKeyWords>new</CodeKeyWords> <ClassName>ExperienceDetails()</ClassName>
+                <br />
+                &#123;
+                <br/>
+                <Tab number={1} />Company = <CodeValues>"Data Annotation Tech"</CodeValues>,
+                <br/>
+                <Tab number={1} />Role = <CodeValues>"Web Developer"</CodeValues>,
+                <br/>
+                <Tab number={1} />Since = <CodeValues>"May 2024"</CodeValues>
+                <br/>
+                &#125;,
+            </>,
+            <br />,
+            <><Tab number={6} />IsOpenToWork = <CodeKeyWords>true</CodeKeyWords></>,
+            <><Tab number={4} />&#125;;</>,
+            <br />,
+            <><Tab number={3} />developer.Introduce();</>,
+            <br />,
+            <><Tab number={3} />developer.BuildSomethingAwesome();</>,
+            <><Tab number={1} />&#125;</>,
+            <>&#125;</>
+        ]} />
 
     );
 }
