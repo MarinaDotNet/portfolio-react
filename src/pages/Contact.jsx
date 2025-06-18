4f0869c06e7c9e1826de24913f9b1f90e2a72f5d
marinadotnet <msichova@outlook.com>
2025-06-18 12:22:09 +0800

    Added page content for page contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,41 @@
+import {NumberedCodeBloc, CodeKeyWords, CodeValues, ClassName, Tab, SingleComment} from "../components/CodeTokens.jsx";
+
 export default function Contact (){
     return (
-        <h1>Contact page from Contact component</h1>
+        <NumberedCodeBloc children={[
+            <><CodeKeyWords>using</CodeKeyWords> Communication.*;</>,
+            <><CodeKeyWords>using</CodeKeyWords> GitHub.Connections;</>,
+            <><CodeKeyWords>using</CodeKeyWords> Trust.Authenticity;</>,
+            <><CodeKeyWords>using</CodeKeyWords> Empathy.InboxZero;</>,
+            <br/>,
+            <><CodeKeyWords>namespace</CodeKeyWords> MarinaDotNet;</>,
+            <br />,
+            <><CodeKeyWords>public static class</CodeKeyWords> Contact</>,
+            <>&#123;</>,
+            <><Tab number={2} /><CodeKeyWords>public static void</CodeKeyWords> <ClassName>ShowContactInfo</ClassName>()</>,
+            <><Tab number={1} />&#123;</>,
+            <><Tab number={2} /><SingleComment>Let's Connect!</SingleComment></>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"--------------------------"</CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Email : <a href="mailto:msichova@outlook.com">msichova@outlook.com"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"GitHub : <a href="https://github.com/MarinaDotNet">MarinaDotNet"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"LinkedIn : <a href="https://www.linkedin.com/in/marina-sichova-333599281?originalSubdomain=au">Marina Sichova"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"WhatsApp : <a href="https://wa.me/+61433881789">Marina Sichova"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Static Portfolio : <a href="https://marinadotnet.github.io">MarinaDotNet"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"React-Vite Portfolio : <a href="https://marinadotnet.github.io/portfolio-react/">MarinaDotNet/porfolio-react"</a></CodeValues>);</>,
+            <br />,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"--------------------------"</CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><SingleComment>Feel free to reach out for collaborations, questions, or jsut to say hi.</SingleComment></>,
+            <><Tab number={1} />&#125;</>,
+            <>&#125;</>
+
+        ]} />
     );
 }
