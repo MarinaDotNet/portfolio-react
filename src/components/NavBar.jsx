80bf5742b4db8181183f7d45195e064b988e3afc
marinadotnet <msichova@outlook.com>
2025-06-25 15:11:19 +0800

    Updated the Navigation Bar content, changed home into home.cs and etc., for better simularity to the Visual Studio UI.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,19 +8,19 @@ export default function NavBar() {
     <div className="nav-bar">
       <div id="nav-buttons">
         <Link to="/">
-          <button className={page === "home" ? "active" : ""}>Home</button>
+          <button className={page === "home" ? "active" : ""}>Home.cs</button>
         </Link>
         <Link to="/projects">
-          <button className={page === "projects" ? "active" : ""}>Projects</button>
+          <button className={page === "projects" ? "active" : ""}>Projects.cs</button>
         </Link>
         <Link to="/skills">
-          <button className={page === "skills" ? "active" : ""}>Skills</button>
+          <button className={page === "skills" ? "active" : ""}>Skills.cs</button>
         </Link>
         <Link to="/aboutme">
-          <button className={page === "aboutme" ? "active" : ""}>About Me</button>
+          <button className={page === "aboutme" ? "active" : ""}>About_Me.cs</button>
         </Link>
         <Link to="/contact">
-          <button className={page === "contact" ? "active" : ""}>Contact</button>
+          <button className={page === "contact" ? "active" : ""}>Contact.cs</button>
         </Link>
       </div>
     </div>
